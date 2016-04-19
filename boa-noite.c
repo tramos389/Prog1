c9e6ad17a1024090e795158043a445790931c846
Tiago Ramos <tramos389@gmail.com>
2016-04-19 01:01:45 +0000

    Meu primeiro commit github

---
+++ boa-noite.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{ 
+    // make faz a compilação e diz se tem erro
+    // ./ executa o arquivo em linux
+    printf("\nboa noite!");
+    printf("\nMeu nome é tiago");
+    printf("\nEste é meu segundo programa c!\n");
+    
+    
+    
+    
+    
+    
+    
+  return 0;  
+}
---
+++ ola-mundo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+
+{ 
+  
+    printf("\n ola mundo! \n");
+    
+    
+    
+    
+    return 0;
+}
